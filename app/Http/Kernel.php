1fde7be159216d29aa6a18bdc5bd12b0f348761e
code-tieumomo <code.tieumomo@gmail.com>
2024-01-23 01:12:31 +0700

    fix: pdf layout

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            VerifyAccessKey::class,
+            // VerifyAccessKey::class,
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
